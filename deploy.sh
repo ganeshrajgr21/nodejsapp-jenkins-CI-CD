21099c1e82bb02dfbf7d212325453e7d41b21b9f
Ganeshraj <37831055+ganeshrajgr21@users.noreply.github.com>
2023-11-08 18:51:08 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
-i#!/bin/bash
+#!/bin/bash
 
 if [[ $GIT_BRANCH == "origin/main" ]]; then
 	  # Build your project
-	  {   ./build.sh
+	   ./build.sh
 
-  docker tag reactapp ganeshraj21/devrepo
-  docker login -u "ganeshraj21" -p "Ganesh@2198"
-  docker push ganeshraj21/devrepo
- }
+ 	    docker tag reactapp ganeshraj21/devrepo
+	    docker login -u "ganeshraj21" -p "Ganesh@2198"
+  	docker push ganeshraj21/reactdevrepo
    else 
 	   [[ $GIT_BRANCH == "origin/master" ]]
 
